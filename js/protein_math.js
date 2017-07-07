148679790f32c6b4e48555ce78386f48101e2f9a
David Sheridan <davidsheridandsgn@gmail.com>
2017-07-07 09:46:07 +0100

    functionality for clear button added

--- js/protein_math.js
+++ js/protein_math.js
@@ -4,16 +4,15 @@ var food_weight = document.querySelector('input.food_weight');
 var calculate = document.querySelector('button.submit');
 var clear = document.querySelector('button.clear');
 var print_answer = document.querySelector('div.print_answer');
-
+var answer = document.createElement('h1')
+    answer.className = "margin-bottom-20";
 
 calculate.addEventListener('click', function(){
   var food;
   var weight;
   var name;
   var calculation;
-  var answer = document.createElement('h1')
-      answer.className = "margin-bottom-20";
-      
+
   // Loop through directory
   for (var i = 0; i < food_directory.length; i ++ ){
 
@@ -24,6 +23,8 @@ calculate.addEventListener('click', function(){
 
     // Check if food name is in directory
     if(name == food.name){
+
+      // TODO: Create a function for this
       calculation = food.protein/100 * weight;
       answer.textContent = calculation;
       print_answer.appendChild(answer);
@@ -33,3 +34,11 @@ calculate.addEventListener('click', function(){
     }
   }
 });
+
+clear.addEventListener('click', function(){
+  // Clear content from input form
+  food_name.value ='';
+  food_weight.value ='';
+  print_answer.removeChild(answer);
+
+})
