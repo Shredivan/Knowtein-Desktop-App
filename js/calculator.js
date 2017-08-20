f82b37c97d205aee97f2bcb7b3d58449a0242711
David Sheridan <davidsheridan@Davids-MacBook-Pro.local>
2017-08-20 13:43:24 +0100

    refactored javascript to remove duplication of variables, algorithim and files

--- js/calculator.js
+++ js/calculator.js
@@ -1,12 +1,26 @@
 // Declare DOM Components
+
+// Inputs
 var food_name = document.querySelector('input.food_name');
+var food_weight = document.querySelector('input.food_weight');
 var exchanges = document.querySelector('input.exchanges');
+
+// Labels
+//  * Check which value user is trying to find and adjust algorithim
+var label_exchanges = document.querySelector('label.food_exchanges');
+var label_weight = document.querySelector('label.food_weight');
+
+
+// Buttons
 var calculate = document.querySelector('button.submit_button');
 var clear = document.querySelector('button.clear_button');
+
+// Print answer to DOM
 var print_answer = document.querySelector('div.print_answer');
 var answer = document.createElement('h1')
     answer.className = "margin-bottom-20";
 
+// Form Event Listener
 calculate.addEventListener('click', function(){
   var food;
   var exchanges;
@@ -20,20 +34,34 @@ calculate.addEventListener('click', function(){
     // Variable to clean up code and make i value more readable
     food = food_directory[i];
     exchanges = parseFloat(food_weight.value);
+    weight = parseFloat(food_weight.value);
     name = food_name.value.toLowerCase();
 
-    // Check if food name is in directory
-    if(name == food.name){
-      calculation = 100/food.protein * exchanges;
-      answer.textContent = calculation;
-      print_answer.appendChild(answer);
 
-      // clear weight input field
-      food_weight.value ='';
+    if(label_exchanges){
+        // Check if food name is in directory
+        if(name == food.name){
+          calculation = 100/food.protein * exchanges;
+          answer.textContent = calculation;
+          print_answer.appendChild(answer);
+
+          // clear weight input field
+          food_weight.value ='';
+        }
+    }else if(label_weight){
+        if(name == food.name){
+           calculation = food.protein/100 * weight;
+           answer.textContent = calculation;
+           print_answer.appendChild(answer);
+
+           // clear weight input field
+           food_weight.value ='';
+        }
     }
   }
 });
 
+// Clear Form
 clear.addEventListener('click', function(){
   // Clear content from input form
   food_name.value ='';
