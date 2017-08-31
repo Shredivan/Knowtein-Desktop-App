f7a16ebf498887542376bb7bf4e637b4c12d57ce
= <davidsheridandsgn@gmail.com>
2017-08-31 17:07:21 +0100

    wrapped script in self calling function to clean up global scope

--- js/calculator.js
+++ js/calculator.js
@@ -1,71 +1,72 @@
-// Declare DOM Components
-// Inputs
-var food_name = document.querySelector('input.food_name');
-var food_weight = document.querySelector('input.food_weight');
-var exchanges = document.querySelector('input.exchanges');
+// Wrap script in sell calling function
+// to prevent pollution of global scope
+(function(){
 
-// Labels
-//  * Check which value user is trying to find and adjust algorithim
-var label_exchanges = document.querySelector('label.food_exchanges');
-var label_weight = document.querySelector('label.food_weight');
+    // Declare DOM Components
+    // Inputs
+    var food_name = document.querySelector('input.food_name');
+    var food_weight = document.querySelector('input.food_weight');
+    var exchanges = document.querySelector('input.exchanges');
 
-// Buttons
-var calculate = document.querySelector('button.submit_button');
-var clear = document.querySelector('button.clear_button');
+    // Labels
+    // Check which value user is trying to find and adjust algorithim
+    var label_exchanges = document.querySelector('label.food_exchanges');
+    var label_weight = document.querySelector('label.food_weight');
 
-// Print answer to DOM
-var print_answer = document.querySelector('div.print_answer');
-var answer = document.createElement('h1')
-    answer.className = "margin-bottom-20";
+    // Buttons
+    var calculate = document.querySelector('button.submit_button');
+    var clear = document.querySelector('button.clear_button');
 
-// Form Event Listener
-calculate.addEventListener('click', function(){
-  var food;
-  var exchanges;
-  var name;
-  var calculation;
+    // Print answer to DOM
+    var print_answer = document.querySelector('div.print_answer');
+    var answer = document.createElement('h1')
+        answer.className = "margin-bottom-20";
 
-  // Loop through directory
-  // TODO: Binary Search to increase efficiency
-  for(var i = 0; i < food_directory.length; i++ ){
+    // Form Event Listener
+    calculate.addEventListener('click', function(){
 
-    // Variable to clean up code and make i value more readable
-    food = food_directory[i];
-    exchanges = parseFloat(food_weight.value);
-    weight = parseFloat(food_weight.value);
-    name = food_name.value.toLowerCase();
+      // Loop through directory
+      // TODO: Binary Search to increase efficiency
+      for(var i = 0; i < food_directory.length; i++ ){
 
-    // If the form is to calculate food weight
-    if(label_exchanges){
-        // Check if food name is in directory
-        if(name == food.name){
-          calculation = 100/food.protein * exchanges;
-          answer.textContent = calculation;
-          print_answer.appendChild(answer);
+        // Variable to clean up code and make i value more readable
+        var food = food_directory[i];
+        var exchanges = parseFloat(food_weight.value);
+        var weight = parseFloat(food_weight.value);
+        var name = food_name.value.toLowerCase();
 
-          // clear weight input field
-          food_weight.value ='';
+        // If the form is to calculate food weight
+        if(label_exchanges){
+            // Check if food name is in directory
+            if(name == food.name){
+              var calculation = 100/food.protein * exchanges;
+              answer.textContent = calculation;
+              print_answer.appendChild(answer);
+
+              // clear weight input field
+              food_weight.value ='';
+            }
         }
-    }
-    // If the form is to calculate food exchanges
-    else if(label_weight){
-        // Check if food name is in directory
-        if(name == food.name){
-           calculation = food.protein/100 * weight;
-           answer.textContent = calculation;
-           print_answer.appendChild(answer);
+        // If the form is to calculate food exchanges
+        else if(label_weight){
+            // Check if food name is in directory
+            if(name == food.name){
+               calculation = food.protein/100 * weight;
+               answer.textContent = calculation;
+               print_answer.appendChild(answer);
 
-           // clear weight input field
-           food_weight.value ='';
+               // clear weight input field
+               food_weight.value ='';
+            }
         }
-    }
-  }
-});
+      }
+    });
 
-// Clear form
-clear.addEventListener('click', function(){
-  // Clear content from input form
-  food_name.value ='';
-  food_weight.value ='';
-  print_answer.removeChild(answer);
-})
+    // Clear form
+    clear.addEventListener('click', function(){
+      // Clear content from input form
+      food_name.value ='';
+      food_weight.value ='';
+      print_answer.removeChild(answer);
+    })
+})();
